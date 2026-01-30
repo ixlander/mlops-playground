3c3d4d2e6cc31fc55866230b61a42cfe2653d822
Maxim Abakarov <abakarovmuslim26@gmail.com>
2026-01-30 22:23:15 +0500

    Update pipeline runner

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,4 +1,7 @@
 from pipelines.training_pipeline import train_pipeline
+from zenml.client import Client
+
 
 if __name__ == "__main__":
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
     train_pipeline(data_path="C:/Users/Admin/Desktop/projects/MLOps/data/olist_customers_dataset.csv")
